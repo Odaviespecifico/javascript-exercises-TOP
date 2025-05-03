97ab37af0c2742b504d4e3af73280921147a6fb8
Odaviespecifico <jak_singer@hotmail.com>
2025-05-02 23:21:52 -0300

    finished exercize 003

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    list_chacters = string.split('')
+    newstring = ''
+    for (let i = 0; i < list_chacters.length; i++) {
+        newstring += list_chacters.at(-1-i)
+    }
+    return newstring
+    console.log(newstring)
 };
-
 // Do not edit below this line
 module.exports = reverseString;
