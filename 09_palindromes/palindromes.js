38a7a608f20059299e2dd0bbac3641764de1ba93
Odaviespecifico <jak_singer@hotmail.com>
2025-05-04 16:22:45 -0300

    finished exercize 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,26 @@
-const palindromes = function () {
+const reverseString = require("../03_reverseString/reverseString");
 
+const palindromes = function (string) {
+    string = string.replaceAll('.','').toUpperCase()
+    string = string.replaceAll(',','')
+    string = string.replaceAll('!','')
+    string = string.replaceAll('.','')
+    string = string.replaceAll(' ','')
+    stringArray = string.split('')
+
+    let reversedstring = []
+    for (let i = 0; i < string.length; i++) { 
+        reversedstring += stringArray.at(-i-1)
+    }
+    stringArray = stringArray.join('')
+    if (stringArray == reversedstring) {
+        return true
+    }
+    else {
+        return false
+    }
 };
 
+palindromes('My perfect string')
 // Do not edit below this line
 module.exports = palindromes;
