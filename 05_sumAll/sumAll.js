fca3908aacb3f6091164107f1cfbd11ccca061c4
Odaviespecifico <jak_singer@hotmail.com>
2025-05-04 15:35:11 -0300

    Finished 05 SumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(start,finish) {
+    let sum = 0
+    if (start < 0 | finish < 0) {
+        return "ERROR"
+    }
+    if (Math.round(start,1) !== start| Math.round(finish,1) !== finish) {
+        return "ERROR"
+    }
+    if (finish < start) {
+        let temp = start
+        start = finish
+        finish = temp
+    }
+    for (let i = start; i <= finish; i++) {
+        sum += i
+    }
+    return sum
 };
 
 // Do not edit below this line
