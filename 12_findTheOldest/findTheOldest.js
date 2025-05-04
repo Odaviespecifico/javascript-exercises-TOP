d4d7adc912137549973a8bc0dd055e91c47b4dd6
Odaviespecifico <jak_singer@hotmail.com>
2025-05-04 16:59:39 -0300

    Finished exercize 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let oldest = ''
+    biggerage = 0
+    people.forEach(element => {
+        let birth = element['yearOfBirth']
+        let death = element['yearOfDeath']
+        console.log(death)
+        if (death == undefined) {
+            death = new Date().getFullYear()
+        }
+        let age = death - birth
 
+        if (age > biggerage) {
+            oldest = element
+            biggerage = age
+        }
+    });
+    return oldest
 };
-
 // Do not edit below this line
 module.exports = findTheOldest;
