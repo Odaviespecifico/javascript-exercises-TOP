0571066a88e05c730ce584e5f9d54953ac74b6e6
Odaviespecifico <jak_singer@hotmail.com>
2025-05-04 15:27:11 -0300

    Finished exercize 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,23 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(array,...remove) {
+    for (let i = 0; i < remove.length; i++) {
+        if (array.indexOf(remove[i]) == -1) {
+            console.log(`did not remove`)
+            continue
+        }
+        else{
+            console.log(remove[i])
+            console.log(`${array.indexOf(remove[i])}`)
+            array.splice(array.indexOf(remove[i]),1)
+        }
+        if (array.indexOf(remove[i]) != -1) {
+            i -= 1
+        }
 
+        console.log(`My array after removing ${array}`)
+    }
+    return array
+   
+};
+removeFromArray([1,2,2,4,5],2)
 // Do not edit below this line
 module.exports = removeFromArray;
