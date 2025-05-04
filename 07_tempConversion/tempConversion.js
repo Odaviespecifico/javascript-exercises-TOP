f94052a7c019a794bc9916cde0bf132bab619bdc
Odaviespecifico <jak_singer@hotmail.com>
2025-05-04 15:51:50 -0300

    Finished exercize 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempCelsius) {
+  return +((((tempCelsius-32)/1.8)).toFixed(1))
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempFahrenheieit) {
+  return +((((tempFahrenheieit)*9/5 + 32)).toFixed(1))
 };
 
 // Do not edit below this line
