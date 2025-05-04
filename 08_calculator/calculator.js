1de9890d2ccc1bbd8e8eee3d60ab3dbe87ff242f
Odaviespecifico <jak_singer@hotmail.com>
2025-05-04 16:05:12 -0300

    finished exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,27 +1,42 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+  let soma = 0
+  for (let index = 0; index < a.length; index++) {
+    const element = +a[index];
+    soma += element
+  }
+  return soma
 };
 
-const multiply = function() {
+const multiply = function(a) {
+  let mult = 1
+  for (let i = 0; i < a.length; i++) {
+    const element = a[i];
+    mult *= element
+  }
+  return mult
+  }
 
+const power = function(a,b) {
+	return a ** b
 };
 
-const power = function() {
-	
+const factorial = function(a) {
+  fac = 1
+	for (let i = a; i >  0; i--) {
+    fac *= i
+  }
+  return fac
 };
 
-const factorial = function() {
-	
-};
-
+console.log(factorial(5))
 // Do not edit below this line
 module.exports = {
   add,
