ad67f0146793e9c92a71d757b911ef4cb3003f6a
Odaviespecifico <jak_singer@hotmail.com>
2025-05-04 16:33:39 -0300

    finished exercize 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,22 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    let previousnum = 1
+    let number = 2
+    let fibonacciNum = 0
+    num = parseInt(num)
+    if (num < 0) {return 'OOPS'}
+    if (num == 0) {return 0}
+    if (num == 1) {return 1}
+    if (num == 2) {return 1}
+    else {
+        for (let i = 2; i <= num-2; i++) {
+            fibonacciNum = number + previousnum
+            previousnum = number
+            number = fibonacciNum
+        }
+    }
+    return fibonacciNum
 };
 
+console.log(fibonacci(4))
 // Do not edit below this line
 module.exports = fibonacci;
